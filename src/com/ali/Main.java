75d329b19512be60bbd29c46d6c1675bd358dc36
ali <membrown@gmail.com>
2014-08-23 23:55:41 -0700

    بسم الله الرحمن الحیم second commit

--- src/com/ali/Main.java
+++ src/com/ali/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("YA Ali");
     }
+
+
+    public boolean f1() {
+        return true;
+    }
+
 }
