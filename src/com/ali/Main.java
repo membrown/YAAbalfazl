54b1d29a4c49697c1b5821a3e91994e16def4fe1
ali <membrown@gmail.com>
2014-09-05 12:06:16 -0700

    بسم الله الرحمن الحیم second commit jalali convertor

--- src/com/ali/Main.java
+++ src/com/ali/Main.java
@@ -1,10 +1,18 @@
 package com.ali;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("YA Ali   ");
+        Calendar calendar = Calendar.getInstance();
+        //calendar.add(Calendar.DAY_OF_YEAR,170);
+        int i = calendar.get(Calendar.MONTH) +1;
+        System.out.println(calendar.get(Calendar.YEAR) + "   " + i + "   " + calendar.get(Calendar.DAY_OF_MONTH));
+        System.out.println(Utilities.getCurrentShamsidate(271));
     }
 
 
---
+++ src/com/ali/Second.java
@@ -0,0 +1,8 @@
+package com.ali;
+
+/**
+ * Created by ali on 9/3/2014.
+ */
+public class Second {
+}
+
